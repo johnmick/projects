8a272706819e6a2f3d03c3ba8e1db1f129e009e8
John Mick <johndmick@gmail.com>
2017-06-05 17:18:02 -0400

    Initial implementation of question generation with checks

--- quiz-me/js/main.js
+++ quiz-me/js/main.js
@@ -1,9 +1,49 @@
 var questions = [
-  { img:       '001.png', 
-    statement: 'The biceps and brachialis muscles synergize to flex the elbow',
+  { 
+    img: './img/quiz-content/103.png', 
+    txt: 'The biceps and brachialis muscles synergize to flex the elbow',
     responses: [
-      { text: "True",  correct: true  },
-      { text: "False", correct: false }
+      { txt: "True",  correct: true  },
+      { txt: "False", correct: false }
     ]
   }
 ];
+
+var container = document.getElementById("quiz");
+
+function clearChildren(target) {
+  while (target.firstChild)
+    target.removeChild(target.firstChild);
+}
+
+function checkAnswer() {
+  console.log(this, this.correct);
+}
+
+function renderQuestion(question) {
+  clearChildren(container);
+
+  var img = document.createElement("IMG");
+  img.src = question.img
+
+  var txt = document.createElement("DIV");
+  txt.innerHTML = question.txt;
+
+
+  var choices = document.createElement("DIV");
+  question.responses.forEach(function(response) {
+    var choice = document.createElement("INPUT");
+    choice.type    = "button";
+    choice.value   = response.txt;
+    choice.correct = response.correct;
+    choice.onclick = checkAnswer;
+    choices.appendChild(choice);
+  });
+
+
+  container.appendChild(img);
+  container.appendChild(txt);
+  container.appendChild(choices);
+
+}
+renderQuestion(questions[0]);
