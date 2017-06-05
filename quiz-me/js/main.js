f8a2f7930ac6af0c059315117415abdd0ca7229a
john <john.d.mick@gmail.com>
2017-06-04 22:02:25 -0400

    Adding boilerplate HTML/JS/CSS for Mobile

---
+++ quiz-me/js/main.js
@@ -0,0 +1,9 @@
+var questions = [
+  { img:       '001.png', 
+    statement: 'The biceps and brachialis muscles synergize to flex the elbow',
+    responses: [
+      { text: "True",  correct: true  },
+      { text: "False", correct: false }
+    ]
+  }
+];
