ae06cc5d7533539af8ed8c5d7525861c61c63443
John Mick <johndmick@gmail.com>
2017-06-05 21:41:07 -0400

    Basic shuffled questions

--- quiz-me/js/main.js
+++ quiz-me/js/main.js
@@ -1,49 +1,116 @@
-var questions = [
-  { 
-    img: './img/quiz-content/103.png', 
-    txt: 'The biceps and brachialis muscles synergize to flex the elbow',
-    responses: [
-      { txt: "True",  correct: true  },
-      { txt: "False", correct: false }
-    ]
-  }
+var container = document.getElementById("quiz");
+var muscles = [
+  undefined,
+  "supraspinatus",
+  "infraspinatus",
+  "teres minor",
+  "teres major",
+  "triceps (long head)",
+  "triceps (short head)",
+  "rhomboid major",
+  "rhomboid minor",
+  "latissimus dorsi",
+  "levator scapulae"
 ];
 
-var container = document.getElementById("quiz");
+var askedMuscles = [];
+
+var numCorrect = 0;
+var numWrong   = 0;
+
+function checkAnswer() {
+  var txt = "";
+  if (this.correct) {
+    txt += "Nice!\n";
+    numCorrect += 1;
+  } else {
+    txt += "Not Quite.\n(It was " + this.actual + ")\n";
+    numWrong += 1;
+  }
+  txt += "\n";
+  txt += "Correct: " + numCorrect + " " + "Incorrect: " + numWrong;
+
+  alert(txt);
+  makeQuestion();
+}
+
+function shuffle(a) {
+    for (let i = a.length; i; i--) {
+        let j = Math.floor(Math.random() * i);
+        [a[i - 1], a[j]] = [a[j], a[i - 1]];
+    }
+}
 
 function clearChildren(target) {
   while (target.firstChild)
     target.removeChild(target.firstChild);
 }
 
-function checkAnswer() {
-  console.log(this, this.correct);
+function randomIntFromInterval(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
 }
 
-function renderQuestion(question) {
+function selectWrongMuscle(correctNumber) {
+  var possibleChoice = correctNumber;
+  while (possibleChoice === correctNumber) {
+    possibleChoice = randomIntFromInterval(1, 10);
+  }
+  return possibleChoice;
+}
+
+function generateQuestionFor(muscleNumber) {
   clearChildren(container);
 
-  var img = document.createElement("IMG");
-  img.src = question.img
+  var imgSrc = "./img/quiz-content/Anatomy/001_m" + muscleNumber + ".png";
+
+  var img          = document.createElement("IMG");
+  img.src          = imgSrc;
+  img.style.width  = "100%";
+  img.style.height = "auto";
 
   var txt = document.createElement("DIV");
-  txt.innerHTML = question.txt;
+  txt.style.textAlign = "center";
+  txt.innerHTML = "Identify the muscle marked with the dot";
 
+  var possibleChoices = [];
+  while (possibleChoices.length < 5) {
+    var possibleChoice = selectWrongMuscle(muscleNumber);
+    if (possibleChoices.indexOf(possibleChoice) === -1) {
+      possibleChoices.push(possibleChoice);
+    }
+  }
+  possibleChoices.push(muscleNumber);
+  shuffle(possibleChoices);
 
   var choices = document.createElement("DIV");
-  question.responses.forEach(function(response) {
-    var choice = document.createElement("INPUT");
+  possibleChoices.forEach(function(mN) {
+    var choice    = document.createElement("INPUT");
     choice.type    = "button";
-    choice.value   = response.txt;
-    choice.correct = response.correct;
+    choice.value   = muscles[mN];
+    choice.actual  = muscles[muscleNumber];
+    choice.correct = mN === muscleNumber;
     choice.onclick = checkAnswer;
+    choice.style.width = "100%";
+    choice.style.marginTop = "5px";
     choices.appendChild(choice);
+    choices.appendChild(document.createElement("BR"));
   });
-
 
   container.appendChild(img);
   container.appendChild(txt);
   container.appendChild(choices);
-
+  askedMuscles.push(muscleNumber);
 }
-renderQuestion(questions[0]);
+
+function makeQuestion() {
+  if (askedMuscles.length === 10) {
+    askedMuscles = [];
+  }
+  var askMuscle = randomIntFromInterval(1, 10);
+  while (askedMuscles.indexOf(askMuscle) !== -1) {
+    askMuscle = randomIntFromInterval(1, 10);
+  }
+  generateQuestionFor(askMuscle);
+}
+makeQuestion();
